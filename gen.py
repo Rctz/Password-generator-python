d57bf17eecc6ff6af8f1a2bfa4f342e2ac501e9f
rctz <storyofdeath2541@gmail.com>
2020-07-27 21:30:07 +0700

    first commit

---
+++ gen.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import string
+import random
+
+char = string.ascii_letters + string.digits + string.punctuation # letter + number + spacial char
+password = ""
+
+try:
+    name = input("Use for: ") # input use for
+    length = int(input("Password length: ")) # input password length
+    for i in range(length):
+        password += random.choice(char) # random
+
+    print(password)
+    with open("password.txt", mode = "a+") as f: # write to .txt
+        f.write(name + "\n")
+        f.write(password + "\n\n")
+
+except ValueError: # if wrong password length
+    print("Error!: please correct password length")
